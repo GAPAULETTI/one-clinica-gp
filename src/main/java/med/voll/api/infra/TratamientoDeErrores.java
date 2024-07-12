2fb551c47041916215785c79559102c6a67b3ffa
gpauletti <gastonpauletti@gmail.com>
2024-07-12 11:11:33 -0300

    Add error managed

---
+++ src/main/java/med/voll/api/infra/TratamientoDeErrores.java
@@ -0,0 +1,28 @@
+package med.voll.api.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class TratamientoDeErrores {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity error404(){
+        return ResponseEntity.notFound().build();
+    }
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity error400(MethodArgumentNotValidException e){
+        var error = e.getFieldErrors().stream().map(DatosErrorValidacion::new).toList();
+        return ResponseEntity.badRequest().body(error);
+    }
+
+    private record DatosErrorValidacion(String campo, String error){
+        public DatosErrorValidacion(FieldError error){
+            this(error.getField(), error.getDefaultMessage());
+        }
+    }
+}
